6de49b41928757624271974070630db16d06b20e
Taehoon Moon <panarch@kaist.ac.kr>
2015-07-09 07:41:51 +0000

    Moved event library sources from Famous#0.3.5 to here

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    EventEmitter: require('./EventEmitter'),
+    EventHandler: require('./EventHandler'),
+    EventFilter: require('./EventFilter'),
+    EventMapper: require('./EventMapper'),
+    EventArbiter: require('./EventArbiter')
+};
